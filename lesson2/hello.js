ecfcf80b394ca3fae44a22627f52c3e6246fe06f
anikshajol <anikshajol@gmail.com>
2023-05-27 22:18:24 +0600

    number method

---
+++ lesson2/hello.js
@@ -0,0 +1,14 @@
+var num = "20";
+
+// var num = parseInt(num);
+var num = Number(num);
+// var num = parseFloat(num);
+
+var num1 = 20.55555;
+
+// var fixed = num1.toFixed(2);
+
+console.log(typeof num);
+console.log(num);
+
+console.log(num1.toPrecision(3));
