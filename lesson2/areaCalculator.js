5c0a0d2e790046981b43ced7c76cb60172be65d8
anikshajol <anikshajol@gmail.com>
2023-05-29 02:25:22 +0600

    operator

---
+++ lesson2/areaCalculator.js
@@ -0,0 +1,11 @@
+// var base = parseFloat(prompt("Enter Base = "));
+// var height = parseFloat(prompt("Enter Height = "));
+// var farn = parseFloat(prompt("Enter Fahrenheit = "));
+
+// var cel = ((farn - 32) * 5) / 9;
+
+var cel = parseFloat(prompt("Enter Celsius = "));
+
+var far = cel * (9 / 5) + 32;
+
+document.write("Fahrenheit = " + far);
---
+++ lesson2/arrithmatic&assignmentOperator.js
@@ -0,0 +1,41 @@
+// arithmatic operator: +,-,*,/, (Modulus %), (exponent ** for power)
+// ++ increment, -- decrement
+
+// assignment operator-> =, +=, -=, *=, /= , %=, **=
+
+var num = 4;
+var num1 = 4;
+
+var add = num + num1;
+
+var sub = num - num1;
+
+var multiplication = num * num1;
+
+var divided = num / num1;
+
+var Modulus = num % num1;
+
+var expo = num ** num1;
+
+var inc = num++;
+
+var dec = num--;
+
+console.log(add, sub, multiplication, divided, Modulus, expo, inc, dec);
+
+// assignment operator-> =, +=, -=, *=, /= , %=, **=
+
+var x = 3;
+
+x += 4; //-> x= x+3;
+
+x -= 1;
+
+x *= 3;
+
+x /= 2;
+
+x %= 9;
+
+console.log(x);
