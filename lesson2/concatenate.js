3df4535f7620e0817b777c4c6aff7f5bd133539a
anikshajol <anikshajol@gmail.com>
2023-05-28 11:45:13 +0600

    library functionForString added

---
+++ lesson2/concatenate.js
@@ -0,0 +1,15 @@
+var firstName = "MD. Anik";
+var lastName = " Hasan";
+
+var fullName = firstName + lastName;
+
+console.log(fullName);
+
+// document.write(fullName + " is my Name");
+
+console.log("My name is " + fullName + " Shajol");
+
+var num1 = 20;
+var num2 = 30;
+
+console.log("num1 = " + num1 + " and " + "num2 =" + num2);
---
+++ lesson2/libraryFunctionForString.js
@@ -0,0 +1,31 @@
+// var text = prompt("Enter Your Name");
+var text = "Bangladesh";
+
+// var len = text.length;
+
+// console.log("Number Of Character: " + len);
+
+// document.write(len);
+
+var chrcterat = text.charAt(3);
+
+console.log(chrcterat);
+
+var upperCase = text.toUpperCase();
+console.log(upperCase);
+
+var lowerCase = text.toLowerCase();
+console.log(lowerCase);
+
+var text1 = "sonar ";
+var text2 = "bangla";
+
+var concatenate = text1.concat(text2).toUpperCase();
+
+console.log(concatenate);
+
+// slice
+
+var sl = text.slice();
+
+console.log(sl);
