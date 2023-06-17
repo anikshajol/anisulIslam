2268235c80fa34afde290c029fb4b0d2ce1e6af8
Jamesrenteri <jamesrenteria76@gmail.com>
2023-06-17 18:09:36 +0600

    function added

---
+++ lesson2/function.js
@@ -0,0 +1,26 @@
+
+
+// function square() {
+//     var num = 5;
+//     var result = num*num;
+//     console.log(result);
+// }
+
+// square()
+
+
+
+function square(num1,num2) {
+   
+    var result = num1*num2;
+    return result
+}
+
+square(6,6)
+
+function addition (x,y){
+    var result= x+y;
+    console.log('Sub: '+ result);
+}
+
+addition(12,4)
--- lesson2/loop.js
+++ lesson2/loop.js
@@ -14,3 +14,4 @@ for(var x=1; x<=5; x++){
    
 }   console.log(sum);
  
+
